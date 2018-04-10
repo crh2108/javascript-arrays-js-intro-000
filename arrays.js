7f30a685a978dfd7deabcf44fe61c959b04803eb
crh2108@gmail.com <crh2108@gmail.com>
2018-04-10 17:28:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 var addElementToBeginningOfArray = (x,y) => {
   [y, ...x]
 }
+
+var destructivelyAddElementToBeginningOfArray = (x, y) => {
+  x.push(y)
+}
